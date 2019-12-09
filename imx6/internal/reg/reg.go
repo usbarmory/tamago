18944baa307c08add992ae8f9f9cccba46755ec5
Andrea Barisani <andrea@inversepath.com>
2019-12-09 16:16:04 +0100

    flush cache on all register methods

--- imx6/internal/reg/reg.go
+++ imx6/internal/reg/reg.go
@@ -17,25 +17,31 @@ import (
 	"github.com/inversepath/tamago/imx6/internal/cache"
 )
 
+// TODO: disable cache for peripheral space instead of cache.FlushData() every
+// time
+
 func Get(reg *uint32, pos int, mask int) uint32 {
-	// TODO: disable cache for peripheral space
 	cache.FlushData()
 	return uint32((int(*reg) >> pos) & mask)
 }
 
 func Set(reg *uint32, pos int) {
+	cache.FlushData()
 	*reg |= (1 << pos)
 }
 
 func Clear(reg *uint32, pos int) {
+	cache.FlushData()
 	*reg &= ^(1 << pos)
 }
 
 func SetN(reg *uint32, pos int, mask int, val uint32) {
+	cache.FlushData()
 	*reg = (*reg & (^(uint32(mask) << pos))) | (val << pos)
 }
 
 func ClearN(reg *uint32, pos int, mask int) {
+	cache.FlushData()
 	*reg &= ^(uint32(mask) << pos)
 }
 
