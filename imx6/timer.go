6ea9101765afbf90764e27dd06166c460c9ac936
Andrej Rosano <andrej.rosano@f-secure.com>
2020-04-27 17:37:49 +0200

    Add system counter initialization
    Memory mapped system counter registers are used to initialize
    the generic timer.

--- imx6/timer.go
+++ imx6/timer.go
@@ -13,10 +13,35 @@ package imx6
 
 import (
 	_ "unsafe"
+
+	"github.com/f-secure-foundry/tamago/imx6/internal/reg"
 )
 
-// nanoseconds
-const refFreq int64 = 1000000000
+const (
+	// p178, Table 2-3, IMX6ULLRM
+	SYS_CNT_BASE uint32 = 0x021dc000
+
+	// p2402, Table D5-1, ARMv7 Architecture Reference Manual
+	CNTCR   = SYS_CNT_BASE
+	CNTSR   = SYS_CNT_BASE + 0x04
+	CNTCV1  = SYS_CNT_BASE + 0x08
+	CNTCV2  = SYS_CNT_BASE + 0x0c
+	CNTFID0 = SYS_CNT_BASE + 0x20 // base frequency
+	CNTFID1 = SYS_CNT_BASE + 0x24 // frequency divider 1
+	CNTFID2 = SYS_CNT_BASE + 0x28 // frequency divider 2
+	CNTID   = SYS_CNT_BASE + 0xfd0
+
+	// p2410, D5.7.2 CNTCR, Counter Control Register, ARMv7 Architecture
+	// Reference Manual
+	CNTCR_FCREQ2 = 10 // frequency = CNTFID0/CNTFID2
+	CNTCR_FCREQ1 = 9  // frequency = CNTFID0/CNTFID1
+	CNTCR_FCREQ0 = 8  // frequency = CNTFID0
+	CNTCR_HDBG   = 1
+	CNTCR_EN     = 0
+
+	// nanoseconds
+	refFreq int64 = 1000000000
+)
 
 var timerFn func() int64
 var timerMultiplier int64
@@ -40,6 +65,14 @@ func initGenericTimers(freq int32) {
 
 	if features.genericTimer && freq != 0 {
 		write_cntfrq(freq)
+		// Set base frequency
+		reg.Write(CNTFID0, uint32(freq))
+		// System counter set to base frequency
+		reg.Set(CNTCR, CNTCR_FCREQ0)
+		// Stop systm counter on debug
+		reg.Set(CNTCR, CNTCR_HDBG)
+		// Start system counter
+		reg.Set(CNTCR, CNTCR_EN)
 	}
 
 	timerFreq = int64(read_cntfrq())
