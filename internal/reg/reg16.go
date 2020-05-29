ff1116b3ee264cf5d24625af674da62d7e9594c9
Andrej Rosano <andrej.rosano@f-secure.com>
2020-05-29 17:53:23 +0200

    Add 16 bit register operations

---
+++ internal/reg/reg16.go
@@ -0,0 +1,93 @@
+// https://github.com/f-secure-foundry/tamago
+//
+// Copyright (c) F-Secure Corporation
+// https://foundry.f-secure.com
+//
+// Use of this source code is governed by the license
+// that can be found in the LICENSE file.
+//
+// +build tamago,arm
+
+// Package reg provides primitives for retrieving and modifying 16 bit hardware
+// registers. As the package sync/atomic does not provide 16 bit support, those
+// functions does not enforce memory ordering to follow the program order.
+package reg
+
+import (
+	"runtime"
+	"time"
+	"unsafe"
+)
+
+func Get16(addr uint32, pos int, mask int) uint16 {
+	reg := (*uint16)(unsafe.Pointer(uintptr(addr)))
+	return (*reg >> pos) & uint16(mask)
+}
+
+func Set16(addr uint32, pos int) {
+	reg := (*uint16)(unsafe.Pointer(uintptr(addr)))
+	*reg |= (1 << pos)
+}
+
+func Clear16(addr uint32, pos int) {
+	reg := (*uint32)(unsafe.Pointer(uintptr(addr)))
+	*reg &= ^(1 << pos)
+}
+
+func SetN16(addr uint32, pos int, mask int, val uint16) {
+	reg := (*uint16)(unsafe.Pointer(uintptr(addr)))
+	*reg = (*reg & (^(uint16(mask) << pos))) | (val << pos)
+}
+
+func ClearN16(addr uint32, pos int, mask int) {
+	reg := (*uint16)(unsafe.Pointer(uintptr(addr)))
+	*reg &= ^(uint16(mask) << pos)
+}
+
+func Read16(addr uint32) uint16 {
+	reg := (*uint16)(unsafe.Pointer(uintptr(addr)))
+	return *reg
+}
+
+func Write16(addr uint32, val uint16) {
+	reg := (*uint16)(unsafe.Pointer(uintptr(addr)))
+	*reg = val
+}
+
+func WriteBack16(addr uint32) {
+	reg := (*uint16)(unsafe.Pointer(uintptr(addr)))
+	*reg |= *reg
+}
+
+func Or16(addr uint32, val uint16) {
+	reg := (*uint16)(unsafe.Pointer(uintptr(addr)))
+	*reg |= val
+}
+
+// Wait waits for a specific register bit to match a value. This function
+// cannot be used before runtime initialization with `GOOS=tamago`.
+func Wait16(addr uint32, pos int, mask int, val uint16) {
+	for Get16(addr, pos, mask) != val {
+		// tamago is single-threaded, give other goroutines a chance
+		runtime.Gosched()
+	}
+}
+
+// WaitFor waits, until a timeout expires, for a specific register bit to match
+// a value. The return boolean indicates whether the wait condition was checked
+// (true) or if it timed out (false). This function cannot be used before
+// runtime initialization with `GOOS=tamago`.
+func WaitFor16(timeout time.Duration, addr uint32, pos int, mask int, val uint16) bool {
+	start := time.Now()
+
+	for Get16(addr, pos, mask) != val {
+		// tamago is single-threaded, give other goroutines a chance
+		runtime.Gosched()
+
+		if time.Since(start) >= timeout {
+			return false
+		}
+	}
+
+	return true
+}
