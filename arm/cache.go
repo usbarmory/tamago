35e08ee1d5ea0502d8105ba7c53815dd9a1245c6
Andrea Barisani <andrea@inversepath.com>
2020-06-05 11:59:21 +0200

    tidying

--- arm/cache.go
+++ arm/cache.go
@@ -15,22 +15,22 @@ func cache_disable()
 func cache_flush_data()
 func cache_flush_instruction()
 
-// CacheEnable activates the ARM MMU instruction and data caches.
+// CacheEnable activates the ARM instruction and data caches.
 func (cpu *CPU) CacheEnable() {
 	cache_enable()
 }
 
-// CacheDisable disables the ARM MMU instruction and data caches.
+// CacheDisable disables the ARM instruction and data caches.
 func (cpu *CPU) CacheDisable() {
 	cache_disable()
 }
 
-// CacheFlushData flushes the ARM MMU data cache.
+// CacheFlushData flushes the ARM data cache.
 func (cpu *CPU) CacheFlushData() {
 	cache_flush_data()
 }
 
-// CacheFlushData flushes the ARM MMU instruction cache.
+// CacheFlushData flushes the ARM instruction cache.
 func (cpu *CPU) CacheFlushInstruction() {
 	cache_flush_instruction()
 }
