26a880763fd3b43876fcc72e4d3b497b545336ea
Andrea Barisani <andrea@inversepath.com>
2021-04-12 19:32:07 +0200

    Revert "restore build constraint for reg.Move"
    This reverts commit 9902ef3ba4de8d8705ba9b3cc2b9df6e701e18ec.

--- internal/reg/reg32.s
+++ internal/reg/reg32.s
@@ -5,10 +5,18 @@
 //
 // Use of this source code is governed by the license
 // that can be found in the LICENSE file.
-//
-// +build tamago,arm
 
-package reg
+// func Move(dst uint32, src uint32)
+TEXT ·Move(SB),$0-8
+	MOVW	dst+0(FP), R0
+	MOVW	src+4(FP), R1
 
-// defined in reg32.s
-func Move(dst uint32, src uint32)
+	// copy src to dst
+	MOVW	(R1), R3
+	MOVW	R3, (R0)
+
+	// zero out src
+	MOVW	$0, R3
+	MOVW	R3, (R1)
+
+	RET
