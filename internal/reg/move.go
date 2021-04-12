9902ef3ba4de8d8705ba9b3cc2b9df6e701e18ec
Andrea Barisani <andrea@inversepath.com>
2021-04-12 19:04:30 +0200

    restore build constraint for reg.Move

--- internal/reg/move.go
+++ internal/reg/move.go
@@ -5,18 +5,10 @@
 //
 // Use of this source code is governed by the license
 // that can be found in the LICENSE file.
+//
+// +build tamago,arm
 
-// func Move(dst uint32, src uint32)
-TEXT ·Move(SB),$0-8
-	MOVW	dst+0(FP), R0
-	MOVW	src+4(FP), R1
+package reg
 
-	// copy src to dst
-	MOVW	(R1), R3
-	MOVW	R3, (R0)
-
-	// zero out src
-	MOVW	$0, R3
-	MOVW	R3, (R1)
-
-	RET
+// defined in reg32.s
+func Move(dst uint32, src uint32)
---
+++ internal/reg/move.s
@@ -0,0 +1,24 @@
+// https://github.com/f-secure-foundry/tamago
+//
+// Copyright (c) F-Secure Corporation
+// https://foundry.f-secure.com
+//
+// Use of this source code is governed by the license
+// that can be found in the LICENSE file.
+
+// +build tamago,arm
+
+// func Move(dst uint32, src uint32)
+TEXT ·Move(SB),$0-8
+	MOVW	dst+0(FP), R0
+	MOVW	src+4(FP), R1
+
+	// copy src to dst
+	MOVW	(R1), R3
+	MOVW	R3, (R0)
+
+	// zero out src
+	MOVW	$0, R3
+	MOVW	R3, (R1)
+
+	RET
