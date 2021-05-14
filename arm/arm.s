e8cdad76cb7171b29b5ad79340abb96bdb3e4c0a
Andrea Barisani <andrea@inversepath.com>
2021-05-14 20:17:17 +0200

    add documentation reference

--- arm/arm.s
+++ arm/arm.s
@@ -9,14 +9,18 @@
 
 // func read_cpsr() uint32
 TEXT ·read_cpsr(SB),$0-4
-	WORD	$0xe10f0000		// mrs r0, CPSR
+	// ARM Architecture Reference Manual - ARMv7-A and ARMv7-R edition
+	// B1.3.3 Program Status Registers (PSRs)
+	WORD	$0xe10f0000 // mrs r0, CPSR
 	MOVW	R0, ret+0(FP)
 
 	RET
 
 // func read_scr() uint32
 TEXT ·read_scr(SB),$0-4
-	MRC	15, 0, R0, C1, C1, 0	// Read SCR into R0
+	// ARM Architecture Reference Manual - ARMv7-A and ARMv7-R edition
+	// B4.1.129 SCR, Secure Configuration Register, Security Extensions
+	MRC	15, 0, R0, C1, C1, 0
 	MOVW	R0, ret+0(FP)
 
 	RET
