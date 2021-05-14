92db64ea6f83e54cbd12f57e66529c91a240ad4e
Andrea Barisani <andrea@inversepath.com>
2021-05-14 20:16:03 +0200

    tidying

--- arm/irq.s
+++ arm/irq.s
@@ -9,10 +9,10 @@
 
 // func irq_enable()
 TEXT ·irq_enable(SB),$0
-	WORD	$0xf10801c0	// CPSIE iaf
+	WORD	$0xf10801c0 // cpsie aif
 	RET
 
 // func irq_disable()
 TEXT ·irq_disable(SB),$0
-	WORD	$0xf10c01c0	// CPSID iaf
+	WORD	$0xf10c01c0 // cpsid aif
 	RET
