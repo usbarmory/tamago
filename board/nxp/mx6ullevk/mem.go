7ff6a6885ec195e63c09adb72e4c4c2ae3e911a4
Andrea Barisani <andrea@inversepath.com>
2021-07-05 17:43:18 +0200

    ensure consistent comment/build flag style

--- board/nxp/mx6ullevk/mem.go
+++ board/nxp/mx6ullevk/mem.go
@@ -6,7 +6,7 @@
 //
 // Use of this source code is governed by the license
 // that can be found in the LICENSE file.
-//
+
 // +build !linkramsize
 
 package mx6ullevk
--- soc/bcm2835/mem.go
+++ soc/bcm2835/mem.go
@@ -5,7 +5,7 @@
 //
 // Use of this source code is governed by the license
 // that can be found in the LICENSE file.
-//
+
 // +build !linkramstart
 
 package bcm2835
