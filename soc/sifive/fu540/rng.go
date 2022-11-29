dd3441a99459330a8b9bf4e6a8c8699b1f1b184f
Andrea Barisani <andrea@inversepath.com>
2022-11-29 22:20:38 +0100

    align FU540 to new rng code

--- soc/sifive/fu540/rng.go
+++ soc/sifive/fu540/rng.go
@@ -10,6 +10,8 @@
 package fu540
 
 import (
+	"encoding/binary"
+	"time"
 	_ "unsafe"
 
 	"github.com/usbarmory/tamago/internal/rng"
@@ -17,16 +19,19 @@ import (
 
 //go:linkname initRNG runtime.initRNG
 func initRNG() {
-	rng.GetRandomDataFn = rng.GetLCGData
+	drbg := &rng.DRBG{}
+	binary.LittleEndian.PutUint64(drbg.Seed[:], uint64(time.Now().UnixNano()))
+	rng.GetRandomDataFn = drbg.GetRandomData
+
 }
 
 // SetRNG allows to override the internal random number generator function used
 // by TamaGo on the FU540 SoC.
 //
-// At runtime initialization the fu540 package selects a timer seeded LCG as
-// the FU540 lacks an entropy source. The LCG is unsuitable for secure random
-// number generation and must therefore be overridden to ensure safe operation
-// of Go `crypto/rand`.
+// At runtime initialization the fu540 package selects a timer seeded with the
+// CPU timer as the FU540 lacks an entropy source. This is unsuitable for
+// secure random number generation and must therefore be overridden to ensure
+// safe operation of Go `crypto/rand`.
 func SetRNG(getRandomData func([]byte)) {
 	rng.GetRandomDataFn = getRandomData
 }
