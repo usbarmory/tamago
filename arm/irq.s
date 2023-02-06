27f12f4aaa8edd52bf669b23d2c30d87aa3c8b03
Andrea Barisani <andrea@inversepath.com>
2023-02-06 22:01:21 +0100

    Revert "tidying"
    This reverts commit 95bf131d288f40f193eff62d6cf4d74c1aa1ada8.

---
+++ arm/irq.s
@@ -0,0 +1,18 @@
+// ARM processor support
+// https://github.com/usbarmory/tamago
+//
+// Copyright (c) WithSecure Corporation
+// https://foundry.withsecure.com
+//
+// Use of this source code is governed by the license
+// that can be found in the LICENSE file.
+
+// func irq_enable()
+TEXT ·irq_enable(SB),$0
+	WORD	$0xf1080180 // cpsie ai
+	RET
+
+// func irq_disable()
+TEXT ·irq_disable(SB),$0
+	WORD	$0xf10c0180 // cpsid ai
+	RET
