f0fa05ecaf5f4780ea0d3cb3bf5eddc46da3b8d9
Andrea Barisani <andrea@inversepath.com>
2024-03-05 15:20:00 +0100

    add redundant boot controls

--- soc/nxp/imx6ul/reset.go
+++ soc/nxp/imx6ul/reset.go
@@ -1,4 +1,4 @@
-// NXP i.MX6UL watchdog support
+// NXP i.MX6UL system reset controller (SRC) support
 // https://github.com/usbarmory/tamago
 //
 // Copyright (c) WithSecure Corporation
@@ -17,16 +17,32 @@ import (
 const (
 	SRC_SCR               = 0x020d8000
 	SCR_WARM_RESET_ENABLE = 0
+
+	SRC_GPR10                    = SRC_SCR + 0x44
+	GPR10_PERSIST_SECONDARY_BOOT = 30
 )
 
-// Reset asserts the global watchdog reset causing the SoC to restart.
+// PersistSecondaryBoot controls whether the primary (false) or secondary boot
+// image (true) should be selected after a software reset.
+func PersistSecondaryBoot(enable bool) {
+	// tested to work correctly only with SetWarmReset(false)
+	reg.SetTo(SRC_GPR10, GPR10_PERSIST_SECONDARY_BOOT, enable)
+}
+
+// SetWarmReset controls whether warm reset sources are enabled (true) or if
+// they should generate a cold reset (false).
+func SetWarmReset(enable bool) {
+	reg.SetTo(SRC_SCR, SCR_WARM_RESET_ENABLE, enable)
+}
+
+// Reset asserts the global watchdog reset causing the SoC to restart with a
+// cold reset.
 //
 // Note that only the SoC itself is guaranteed to restart as, depending on the
 // board hardware layout, the system might remain powered (which might not be
 // desirable). See respective board packages for cold reset options.
 func Reset() {
-	// disable warm reset
-	reg.Clear(SRC_SCR, SCR_WARM_RESET_ENABLE)
+	SetWarmReset(false)
 
 	// assert software reset
 	WDOG1.SoftwareReset()
