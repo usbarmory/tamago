022c3c47a82add8be58673933e62acffb8524a93
Andrea Barisani <andrea@inversepath.com>
2025-01-02 16:30:31 +0100

    working amd64 long mode

--- amd64/init.s
+++ amd64/init.s
@@ -11,10 +11,20 @@
 
 #define MSR_EFER 0xc0000080
 
-#define PML4T 0x1000 // Page Map Level 4 Table       (512GB entries)
-#define PDPT  0x2000 // Page Directory Pointer Table   (1GB entries)
-#define PDT   0x3000 // Page Directory Table           (2MB entries)
-#define PT    0x4000 // Page Table                     (4kB entries)
+#define PML4T 0x1000	// Page Map Level 4 Table       (512GB entries)
+#define PDPT  0x2000	// Page Directory Pointer Table   (1GB entries)
+#define PDT   0x3000	// Page Directory Table           (2MB entries)
+#define PT    0x4000	// Page Table                     (4kB entries)
+
+// Global Descriptor Table
+DATA	gdt<>+0x00(SB)/8, $0x0000000000000000	// null descriptor
+DATA	gdt<>+0x08(SB)/8, $0x00209a0000000000	// code descriptor (x/r)
+DATA	gdt<>+0x10(SB)/8, $0x0000920000000000	// data descriptor (r/w)
+GLOBL	gdt<>(SB),8,$24
+
+DATA	gdtptr<>+0x00(SB)/2, $0x1800		// GDT Limit
+DATA	gdtptr<>+0x02(SB)/8, $gdt<>(SB)		// GDT Base Address
+GLOBL	gdtptr<>(SB),8,$10
 
 TEXT cpuinit(SB),NOSPLIT|NOFRAME,$0
 	// Disable interrupts
@@ -37,46 +47,45 @@ TEXT cpuinit(SB),NOSPLIT|NOFRAME,$0
 
 	// PML4T[0] = PDPT
 	MOVL	$PML4T, DI
-	MOVL	$(PDPT | 1<<1 | 1<<0), (DI)	// set R/W, P
+	MOVL	$(PDPT | 1<<1 | 1<<0), (DI)			// set R/W, P
 
-	// PDPT[0] = PDT
 	MOVL	$PDPT, DI
-	MOVL	$(PDT | 1<<1 | 1<<0), (DI)	// set R/W, P
-
-	// PDT[0] = PT
-	MOVL	$PDT, DI
-	MOVL	$(PT | 1<<1 | 1<<0), (DI)	// set R/W, P
-
-	MOVL	$PT, DI
-	MOVL	$(1<<1 | 1<<0), AX		// set R/W, P
-build_table:
-	MOVL	AX, (DI)
-	ADDL	$0x1000, AX
-	ADDL	$8, DI
-	CMPL	AX, $0x200000
-	JB	build_table
+	MOVQ	$(0x00000000 | 1<<7 | 1<<1 | 1<<0), (DI)	// set PS, R/W, P
 
 	// Enter long mode
 
-	MOVL	$(1<<7 | 1<<5), AX		// set PGE, PAE
+	MOVL	$(1<<7 | 1<<5), AX	// set PGE, PAE
 	MOVL	AX, CR4
 
 	MOVL	$MSR_EFER, CX
 	RDMSR
-	ORL	$(1<<8), AX			// set LME
+	ORL	$(1<<8), AX		// set LME
 	WRMSR
 
 	MOVL	CR0, BX
-	ORL	$(1<<31 | 1<<0), BX		// set PG, PE
+	ORL	$(1<<31 | 1<<0), BX	// set PG, PE
 	MOVL	BX, CR0
 
-	// Hello World (debug)
-	MOVL	$0xb8000, DI
-	MOVQ	$0x1F6C1F6C1F651F48, AX
-	MOVQ	AX, (DI)
-	MOVQ	$0x1F6F1F571F201F6F, AX
-	ADDQ	AX, 1(DI)
-	MOVQ	$0x1F211F641F6C1F72, AX
-	MOVQ	AX, 2(DI)
+	// Set Global Descriptor Table
 
+	CALL	·getPC<>(SB)
+	MOVL	$gdtptr<>(SB), BX	// 32-bit mode: only PC offset is copied
+	ADDL	$6, AX
+	ADDL	BX, AX
+	LGDT	(AX)
+
+	CALL	·getPC<>(SB)
+	MOVL	$·flush<>(SB), BX	// 32-bit mode: only PC offset is copied
+	ADDL	$6, AX
+	ADDL	BX, AX
+
+	PUSHQ	$0x08
+	PUSHQ	AX
+	RETFQ
+
+TEXT ·flush<>(SB),NOSPLIT|NOFRAME,$0
 	JMP	_rt0_tamago_start(SB)
+
+TEXT ·getPC<>(SB),NOSPLIT|NOFRAME,$0
+	POPQ	AX
+	CALL	AX
