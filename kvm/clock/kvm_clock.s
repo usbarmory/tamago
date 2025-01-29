2fead141f3d7d74543337e79e7cf71ddf39ca484
Andrea Barisani <andrea@inversepath.com>
2025-01-29 14:29:01 +0100

    tidying

--- kvm/clock/kvm_clock.s
+++ kvm/clock/kvm_clock.s
@@ -7,12 +7,13 @@
 // that can be found in the LICENSE file.
 
 #include "go_asm.h"
+#include "textflag.h"
 
 // holder for struct pvclock_vcpu_time_info
 DATA	pvclock<>+0x00(SB)/8, $0x0000000000000000
 DATA	pvclock<>+0x08(SB)/8, $0x0000000000000000
 DATA	pvclock<>+0x10(SB)/8, $0x0000000000000000
-GLOBL	pvclock<>(SB),8,$32
+GLOBL	pvclock<>(SB),RODATA,$32
 
 // func pvclock(msr uint32) uint32
 TEXT ·pvclock(SB),$8
