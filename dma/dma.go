00be145fe12a137e0f210791912c2c86983c8092
Andrea Barisani <andrea@inversepath.com>
2025-03-19 18:20:27 +0100

    fix and simplify DMA region check

--- dma/dma.go
+++ dma/dma.go
@@ -35,10 +35,7 @@ func NewRegion(addr uint, size int, unsafe bool) (r *Region, err error) {
 	ramStart := uint(rs)
 	ramEnd := uint(re)
 
-	if !unsafe &&
-		(ramStart >= start && ramStart < end ||
-			ramEnd > start && ramEnd < end ||
-			start >= ramStart && end < ramEnd) {
+	if !unsafe && !(start >= ramEnd || start < ramStart && end <= ramStart) {
 		return nil, fmt.Errorf("DMA within Go runtime memory (%#x-%#x) is not allowed", ramStart, ramEnd)
 	}
 
