526dd84d62be3c16e50eb10eb3d79763e897bfe7
Andrea Barisani <andrea@inversepath.com>
2025-07-23 15:12:15 +0200

    tidying

--- arm/exception.s
+++ arm/exception.s
@@ -102,30 +102,6 @@ TEXT ·prefetchAbortHandler(SB),NOSPLIT|NOFRAME,$0
 TEXT ·dataAbortHandler(SB),NOSPLIT|NOFRAME,$0
 	EXCEPTION(const_DATA_ABORT, ·systemException, 8, R12, 56)
 
-TEXT ·irqHandler(SB),NOSPLIT|NOFRAME,$0
-	// remove exception specific LR offset
-	SUB	$4, R14, R14
-
-	// save caller registers
-	MOVM.DB.W	[R0-R12, R14], (R13)	// push {r0-r12, r14}
-
-	// wake up IRQ handling goroutine
-	MOVW	·irqHandlerG(SB), R0
-	CMP	$0, R0
-	B.EQ	done
-	CALL	runtime·WakeG(SB)
-
-	// the IRQ handling goroutine is expected to unmask IRQs
-	WORD	$0xe14f0000			// mrs r0, SPSR
-	ORR	$1<<7, R0			// mask IRQs
-	WORD	$0xe169f000			// msr SPSR, r0
-done:
-	// restore caller registers
-	MOVM.IA.W	(R13), [R0-R12, R14]	// pop {r0-r12, r14}
-
-	// restore PC from LR and mode
-	MOVW.S	R14, R15
-
 TEXT ·fiqHandler(SB),NOSPLIT|NOFRAME,$0
 	EXCEPTION(const_FIQ, ·systemException, 4, R7, 36)
 
