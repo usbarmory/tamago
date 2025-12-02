2ab14c51da389bd7be8f0cb8a6f21b268df9c032
Andrea Barisani <andrea@inversepath.com>
2025-12-02 22:40:58 +0100

    tidying

--- board/cloud_hypervisor/vm/vm.go
+++ board/cloud_hypervisor/vm/vm.go
@@ -7,7 +7,7 @@
 // that can be found in the LICENSE file.
 
 // Package vm provides hardware initialization, automatically on import, for a
-// Cloud Hypervisor virtual machine configured with a single x86_64 core.
+// Cloud Hypervisor virtual machine configured with one or more x86_64 cores.
 //
 // This package is only meant to be used with `GOOS=tamago GOARCH=amd64` as
 // supported by the TamaGo framework for bare metal Go, see
