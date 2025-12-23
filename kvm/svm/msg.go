5e9e7a8fa37939da92d8d3b934cc5554ad8b40d9
Andrea Barisani <andrea@inversepath.com>
2025-12-23 12:48:07 +0100

    tidying

--- kvm/svm/msg.go
+++ kvm/svm/msg.go
@@ -57,12 +57,12 @@ func (h *MessageHeader) Bytes() []byte {
 
 // Seq returns the 64-bit sequence number for this message.
 func (h *MessageHeader) Seq() uint64 {
-	return binary.LittleEndian.Uint64(h.SeqNo[0:8])
+	return binary.LittleEndian.Uint64(h.SeqNo[:])
 }
 
 // SetSeq sets the 64-bit sequence number for this message.
 func (h *MessageHeader) SetSeq(seq uint64) {
-	binary.LittleEndian.PutUint64(h.SeqNo[0:8], seq)
+	binary.LittleEndian.PutUint64(h.SeqNo[:], seq)
 }
 
 func seal(key, nonce, plaintext, additionalData []byte) (ciphertext []byte, err error) {
@@ -102,7 +102,7 @@ func unseal(key, nonce, ciphertext, additionalData []byte) (plaintext []byte, er
 func (b *GHCB) sealMessage(hdr *MessageHeader, plaintext, key []byte) (msg []byte, err error) {
 	// update header
 	hdr.MessageSize = uint16(len(plaintext))
-	binary.LittleEndian.PutUint64(hdr.SeqNo[:], b.seqNo)
+	hdr.SetSeq(b.seqNo)
 
 	// encrypt request
 	ciphertext, err := seal(key, hdr.SeqNo[0:12], plaintext, hdr.Bytes()[48:])
