f2fd9792100ff9f263bb5344ed3ac4107c1423c2
Andrea Barisani <andrea@inversepath.com>
2026-01-20 18:57:13 +0100

    add amd64 MMU reconfiguration helpers

---
+++ amd64/mmu.go
@@ -0,0 +1,101 @@
+// AMD64 processor support
+// https://github.com/usbarmory/tamago
+//
+// Copyright (c) The TamaGo Authors. All Rights Reserved.
+//
+// Use of this source code is governed by the license
+// that can be found in the LICENSE file.
+
+package amd64
+
+import (
+	"github.com/usbarmory/tamago/bits"
+	"github.com/usbarmory/tamago/internal/reg"
+)
+
+const CR0_WP = 16
+
+// Memory region attributes
+const (
+	TTE_P  uint64 = (1 << 0)
+	TTE_PS uint64 = (1 << 7)
+)
+
+// Page levels
+const (
+	PML4 = 4
+	PDPT = 3
+	PD   = 2
+	PT   = 1
+
+	// AMD64 Architecture Programmer’s Manual
+	// Figure 5-17. 4-Kbyte Page Translation—Long Mode 4-Level Paging
+	indexPML4    = 39
+	indexPDPT    = 30
+	indexPD      = 21
+	indexPT      = 12
+	tableEntries = 512
+	indexMask    = 0x1ff
+	addrMask     = 0x000ffffffffff000
+)
+
+func flush_tlb()
+func read_cr0() uint64
+func write_cr0(val uint64)
+func read_cr3() uint64
+
+// FlushTLBs invalidates non-global page translations.
+func (cpu *CPU) FlushTLBs() {
+	flush_tlb()
+}
+
+// SetWriteProtect configures the Write Protect (WP) bit in Control Register 0
+// (CR0).
+func (cpu *CPU) SetWriteProtect(enable bool) {
+	cr0 := read_cr0()
+	bits.SetTo64(&cr0, CR0_WP, enable)
+	write_cr0(cr0)
+}
+
+// FindPTE returns the Page Table Entry (PTE) offset, level and base address
+// for a given address. A non-zero address bit length can override the default
+// value to support implementation specific limitations (e.g. C-Bit).
+func (cpu *CPU) FindPTE(addr uint64, bitLen int) (pte uint64, level int, page uint64) {
+	indices := [4]uint64{
+		(addr >> indexPML4) & indexMask,
+		(addr >> indexPDPT) & indexMask,
+		(addr >> indexPD) & indexMask,
+		(addr >> indexPT) & indexMask,
+	}
+
+	mask := uint64(addrMask)
+
+	if bitLen > 0 {
+		mask &= uint64(1<<bitLen) - 1
+	}
+
+	pml4 := read_cr3()
+	tableAddr := pml4 & mask
+
+	for i := 0; i < 4; i++ {
+		level = 4 - i
+		off := tableAddr + indices[i]*8
+		entry := reg.Read64(off)
+
+		if entry&TTE_P == 0 {
+			return 0, level, entry & mask
+		}
+
+		if (level == PDPT || level == PD) && (entry&TTE_PS != 0) {
+			return off, level, entry & mask
+		}
+
+		if level == PT {
+			return off, PT, entry & mask
+		}
+
+		tableAddr = entry & mask
+	}
+
+	return 0, 0, 0
+}
