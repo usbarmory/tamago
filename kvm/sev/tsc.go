f2e070fe70eb41245894d749db849e7f9103b551
Andrea Barisani <andrea@inversepath.com>
2026-01-21 22:08:01 +0100

    authenticated TSC information request

--- kvm/sev/tsc.go
+++ kvm/sev/tsc.go
@@ -8,14 +8,53 @@
 
 package sev
 
-// SecureTSC performs a read of the time-stamp counter through the
-// Guest-Hypervisor Communication Block.
-func (b *GHCB) SecureTSC() (tsc uint64, err error) {
-	if err = b.Exit(RDTSC, 0, 0); err != nil {
+import (
+	"encoding/binary"
+	"fmt"
+)
+
+const (
+	MSG_TSC_INFO_REQ = 17
+	MSG_TSC_INFO_RES = 18
+)
+
+// TSCInfo represents an AMD SEV-SNP TSC Information Response
+// (SEV Secure Nested Paging Firmware ABI Specification
+// Table 39: MSG_TSC_INFO_RES Message Structure).
+type TSCInfo struct {
+	Status         uint32
+	_              uint32
+	GuestTSCScale  uint64
+	GuestTSCOffset uint64
+	TSCFactor      uint32
+	_              [100]byte
+}
+
+func (r *TSCInfo) unmarshal(buf []byte) (err error) {
+	_, err = binary.Decode(buf, binary.LittleEndian, r)
+	return
+}
+
+// SecureTSC sends an AMD SEV-SNP guest request for TSC information. The
+// arguments represent guest provided request parameters, the VM Communication
+// Key (see [SNPSecrets.VMPCK]) payload and index for encrypting the request.
+func (b *GHCB) SecureTSC(key []byte, index int) (res *TSCInfo, err error) {
+	var buf []byte
+
+	req := make([]byte, 128)
+	res = &TSCInfo{}
+
+	if buf, err = b.GuestRequest(index, key, req, MSG_TSC_INFO_REQ); err != nil {
 		return
 	}
 
-	vmgexit()
+	if err = res.unmarshal(buf); err != nil {
+		return nil, fmt.Errorf("could not parse response, %v", err)
+	}
 
-	return b.read(RDX)<<32 | b.read(RAX), nil
+	if res.Status != 0 {
+		return nil, fmt.Errorf("request error, %#x", res.Status)
+	}
+
+	return
 }
