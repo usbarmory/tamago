a56f03727d4a940d6787b527f2689071872bd97a
Andrea Barisani <andrea@inversepath.com>
2026-02-02 17:24:39 +0100

    tidying

--- goos/goos.go
+++ goos/goos.go
@@ -1,3 +1,11 @@
+// Bare metal Go runtime support
+// https://github.com/usbarmory/tamago
+//
+// Copyright (c) The TamaGo Authors. All Rights Reserved.
+//
+// Use of this source code is governed by the license
+// that can be found in the LICENSE file.
+
 // Package goos describes required, as well as optional, runtime
 // functions/variables for custom GOOS implementations as supported by the
 // GOOSPKG variable.
