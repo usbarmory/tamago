c57f7abb06e5ac1a7777341a6aba66d0f03c811f
Andrea Barisani <andrea@inversepath.com>
2026-03-13 20:24:11 +0100

    add constants for upcoming support of reduced memory allocation

--- goos/mem.go
+++ goos/mem.go
@@ -12,6 +12,14 @@ package goos
 
 // Required constants.
 const (
+	// ArenaBaseOffset is the pointer value that corresponds to index 0 in
+	// the heap arena map (see runtime.arenaBaseOffset).
+	ArenaBaseOffset = 0
+
+	// HeapAddrBits defines the number of bits in a heap address (see
+	// runtime.heapAddrBits).
+	HeapAddrBits = 32
+
 	// LogHeapArenaBytes defines the size of a runtime heap arena in log_2
 	// bytes (see runtime.logHeapArenaBytes).
 	LogHeapArenaBytes = (2+20)
@@ -23,4 +31,8 @@ const (
 	// MinPhysPageSize is a lower-bound on the physical page size (see
 	// runtime.minPhysPageSize).
 	MinPhysPageSize = 4096
+
+	// StackSystem is a number of additional bytes to add to each stack
+	// below the usual guard area.
+	StackSystem = 0
 )
