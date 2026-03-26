7eb5781ba819efeea10a7236c801f440928d54b1
Andrea Barisani <andrea@inversepath.com>
2026-03-26 11:14:41 +0100

    gofmt

--- soc/aifoundry/etsoc1/minion/exception.go
+++ soc/aifoundry/etsoc1/minion/exception.go
@@ -22,16 +22,16 @@ func vector(fn riscv64.ExceptionHandler) uint64 {
 
 func encodeLongJump(ptr, pc uint64) uint64 {
 	off := uint64(ptr) - uint64(pc)
-	hi := uint32(off + 0x800) >> 12
+	hi := uint32(off+0x800) >> 12
 	lo := uint32(off & 0xfff)
 
 	// Volume I: RISC-V Unprivileged ISA V20191213
 	// RV32I Base Instruction Set
 	r := uint32(6)
 	auipc := (hi << 12) | (r << 7) | uint32(0b10111)
-	jalr := (lo << 20) | (r << 15) |  uint32(0b1100111)
+	jalr := (lo << 20) | (r << 15) | uint32(0b1100111)
 
-	return uint64(auipc) | uint64(jalr) << 32
+	return uint64(auipc) | uint64(jalr)<<32
 }
 
 func alignExceptionHandler() {
