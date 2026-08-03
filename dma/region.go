18b653c3dd29f1319fa3a395fe06f9da21e3c0da
Andrea Barisani <andrea@inversepath.com>
2026-08-03 23:15:22 +0200

    fix reservation check

--- dma/region.go
+++ dma/region.go
@@ -147,7 +147,7 @@ func (r *Region) Alloc(buf []byte, align int) (addr uint) {
 		return 0
 	}
 
-	if res, addr := Reserved(buf); res {
+	if res, addr := r.Reserved(buf); res {
 		return addr
 	}
 
@@ -179,7 +179,7 @@ func (r *Region) Read(addr uint, off int, buf []byte) {
 		return
 	}
 
-	if res, _ := Reserved(buf); res {
+	if res, _ := r.Reserved(buf); res {
 		return
 	}
 
