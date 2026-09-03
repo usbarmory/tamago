bc4e5ab0743acc2348e8f3b71e57532c3083fc75
Luka Perkov <luka.perkov@sartura.hr>
2026-09-03 22:32:22 +0200

    internal/reg: add bounded 8-bit wait

--- internal/reg/reg8.go
+++ internal/reg/reg8.go
@@ -7,6 +7,25 @@
 
 package reg
 
+import (
+	"runtime"
+	"time"
+)
+
 // defined in reg_*.s
 func Read8(addr uint32) uint8
 func Write8(addr uint32, val uint8)
+
+func WaitFor8(timeout time.Duration, addr uint32, pos int, mask int, val uint8) bool {
+	start := time.Now()
+
+	for Read8(addr)>>pos&uint8(mask) != val {
+		runtime.Gosched()
+
+		if time.Since(start) >= timeout {
+			return false
+		}
+	}
+
+	return true
+}
