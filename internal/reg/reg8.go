878882ff29890189bf05de72d27e1cf8005d18b9
Andrea Barisani <andrea@inversepath.com>
2026-09-04 10:27:18 +0200

    align call style

--- internal/reg/reg8.go
+++ internal/reg/reg8.go
@@ -16,10 +16,15 @@ import (
 func Read8(addr uint32) uint8
 func Write8(addr uint32, val uint8)
 
+func GetN8(addr uint32, pos int, mask int) uint8 {
+	val := Read8(addr)
+	return (val >> pos) & uint8(mask)
+}
+
 func WaitFor8(timeout time.Duration, addr uint32, pos int, mask int, val uint8) bool {
 	start := time.Now()
 
-	for Read8(addr)>>pos&uint8(mask) != val {
+	for GetN8(addr, pos, mask) != val {
 		runtime.Gosched()
 
 		if time.Since(start) >= timeout {
